020d24e42e428d11c22e3fee038149450ed2186d
unknown <msormaz@MILANSORMAZ.planetsoft.ba>
2020-12-04 21:50:33 +0100

    Initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get } from '@nestjs/common';
+
+
+@Controller()
+export class AppController {
+   @Get()
+  getHello(): string {
+    return 'Hello world';
+  }
+
+
+
+   @Get('world')
+  getWorld(): string {
+    return 'World';
+  }
+}
