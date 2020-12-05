c33f0cd80b8fcba22c0b5e8744da24a572f6f241
unknown <msormaz@MILANSORMAZ.planetsoft.ba>
2020-12-05 18:26:48 +0100

    Create the administrator table aservices

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
-
+import { Administrator } from 'entities/administrator.entity';
+import { AdministratorService } from './services/administrator/administrator.service';
 
 @Controller()
 export class AppController {
-   @Get()
+  constructor(
+    private administratorService: AdministratorService
+) { }
+
+@Get()
   getIndex(): string {
     return 'Home page';
+  }
+
+@Get('api/administrator')
+  getAllAdmins(): Promise<Administrator[]> {
+    return this.administratorService.getAll();
   }
 }
