00fe41bcbf1b0de83c76153391fc9e2731401a5e
unknown <msormaz@MILANSORMAZ.planetsoft.ba>
2020-12-08 09:47:20 +0100

    Dodano

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,6 @@ import {TypeOrmModule} from '@nestjs/typeorm';
 import { DatabaseConfiguration } from 'config/database.configuration';
 import { Administrator } from 'entities/administrator.entity';
 import { AdministratorService } from './services/administrator/administrator.service';
-/*import { ArticleFeature } from 'entities/article-feature.entity';
-import { ArticlePrice } from 'entities/article-price.entity';
-import { Article } from 'entities/Article.entity';
-import { CartArticle } from 'entities/cart-article.entity';
-import { Cart } from 'entities/cart.entity';
-import { Category } from 'entities/category.entity';
-import { Feature } from 'entities/feature.entity';
-import { Order } from 'entities/order.entity';
-import { Phote } from 'entities/phote.entity';
-import { User } from 'entities/user.entity';*/
 import { AdministratorController } from './controllers/api/administrator.controller';
 
 
@@ -28,17 +18,8 @@ import { AdministratorController } from './controllers/api/administrator.control
       password: DatabaseConfiguration.password,
       database: DatabaseConfiguration.database,
       entities: [
-        Administrator,
-        /*ArticleFeature,
-        ArticlePrice,
-        Article,
-        CartArticle,
-        Cart,
-        Category,
-        Feature,
-        Order,
-        Phote,
-        User,*/
+        Administrator
+
       ]
 
     }),
